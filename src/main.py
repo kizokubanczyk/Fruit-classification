723df6be8e3410d75d62a4843f8b060a8b6532f9
Patryk Jura <you@example.com>
2024-09-14 21:09:14 +0200

    Added dataSet

--- src/main.py
+++ src/main.py
@@ -1,23 +1,15 @@
 import click
 import yaml
-from src.importData_and_dataLoader.importData import importData
-
+from dataset_and_dataloader.dataset import ClassificationDataset
 
 @click.command()
 @click.option('--config', '-c', default='../config.yaml', help='Path to the configuration file')
 def run(config) -> None:
     with (open(config, 'r') as file):
         config_data = yaml.safe_load(file)
-        config_path = '../config.yaml'
 
-        path_train_data = config_data.get('path_train_data')
-        path_test_data = config_data.get('path_test_data')
-        path_predict_data = config_data.get('path_predict_data')
-
-
-        importData(config_path, path_train_data)
-
-
+        path_data_train = config_data.get('path_data_train_for_main')
+        classificationDataset = ClassificationDataset(path_data_train)
 
 
 if __name__ == "__main__":
